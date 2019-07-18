908543e5f11dfdc3b9d24aa5f565f19263aa23cc
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-18 19:14:31 +0800

    [YJT] pass JPA test of Story1 AC1

--- src/test/java/com/thoughtworks/parking_lot/repositoryTest/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/repositoryTest/ParkingLotControllerTest.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.parking_lot.repositoryTest;
 
 import com.thoughtworks.parking_lot.dao.ParkingLotRepository;
+import com.thoughtworks.parking_lot.entity.ParkingLot;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
@@ -16,6 +20,12 @@ public class ParkingLotControllerTest {
 
     @Test
     void should_save_parking_lot() {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName("TestSave");
 
+        parkingLotRepository.save(parkingLot);
+        ParkingLot fetchLot = parkingLotRepository.findAllByName("TestSave").get(0);
+        
+        assertNotNull(fetchLot);
     }
 }
